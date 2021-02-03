34161b609f9f0f52ab233e8c5d83b6ce1d2fc313
dromero1 <romeromillandavidandres@gmail.com>
2021-02-03 17:24:07 -0500

    Update power_fis.m

--- src/power_fis.m
+++ src/power_fis.m
@@ -8,22 +8,22 @@ fis = mamfis('Name','Thermal condition');
 categories = {'normal','low','medium','high','significant'};
 
 % Inputs
-fis = addInput(fis,[0 3001],'Name','ethylene');
-fis = addInput(fis,[0 10001],'Name','ethane');
+fis = addInput(fis,[0 300],'Name','ethylene');
+fis = addInput(fis,[0 300],'Name','ethane');
 
 % Ethylene's membership functions
 fis = addMF(fis,'ethylene','gauss2mf',[0.0001 0 11.8 10],'Name',categories{1});
 fis = addMF(fis,'ethylene','gauss2mf',[4.75 26 7.64 50],'Name',categories{2});
-fis = addMF(fis,'ethylene','gauss2mf',[9.34 70 20.957 100],'Name',categories{3});
-fis = addMF(fis,'ethylene','gauss2mf',[8.981 120 15.61 150],'Name',categories{4});
-fis = addMF(fis,'ethylene','gauss2mf',[21.86 160 0.1 3000],'Name',categories{5});
+fis = addMF(fis,'ethylene','gauss2mf',[9.34 70 20.957 90],'Name',categories{3});
+fis = addMF(fis,'ethylene','gauss2mf',[8.981 105 15.61 135],'Name',categories{4});
+fis = addMF(fis,'ethylene','gauss2mf',[21.86 145 0.1 400],'Name',categories{5});
 
 % Ethane's membership functions
 fis = addMF(fis,'ethane','gauss2mf',[0.0001 0 7.91 10],'Name',categories{1});
 fis = addMF(fis,'ethane','gauss2mf',[11.87 25 17.54 45],'Name',categories{2});
 fis = addMF(fis,'ethane','gauss2mf',[17.54 65 18.42 85],'Name',categories{3});
 fis = addMF(fis,'ethane','gauss2mf',[7.89 100 9.8 125],'Name',categories{4});
-fis = addMF(fis,'ethane','gauss2mf',[11.98 135 0.1 10000],'Name',categories{5});
+fis = addMF(fis,'ethane','gauss2mf',[11.98 135 0.1 400],'Name',categories{5});
 
 % Output
 fis = addOutput(fis,[0 100],'Name','criticality-level');
@@ -105,7 +105,11 @@ xlabel('criticality level');
 ylabel('Degree of membership');
 legend(categories);
 
+% Output surface
+figure;
+gensurf(fis);
+
 %% Evaluation
 
 % Sample evaluation
-evalfis(fis,[50 120])
+evalfis(fis,[100 75])
