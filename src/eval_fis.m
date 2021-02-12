f6380f8e72532bda013e3f907586d17a5c96583d
dromero1 <romeromillandavidandres@gmail.com>
2021-02-11 20:09:12 -0500

    test set evaluation

--- src/eval_fis.m
+++ src/eval_fis.m
@@ -4,5 +4,22 @@ clc;
 % Load FIS
 fis = readfis('../build/thermal-condition');
 
+% Load test set
+load('../data/test.mat')
+
+% Extract raw structures
+X = test(:,[1 2]);
+y_ref = test(:,end);
+
+% Scale inputs and outputs 
+X = X./180;
+y_ref = y_ref./100;
+
 % Sample evaluation
-evalfis(fis,[1 1])
+y_hat = evalfis(fis,X);
+
+% Mean Absolute Error
+mae = mean(abs(y_hat-y_ref));
+
+% Display
+fprintf('MAE = %0.4f\n',mae);
